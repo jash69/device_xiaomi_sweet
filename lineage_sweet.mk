e6d8a8d4f38059d14b6d5d80aea715fd63684f1b
jash <jashvakharia@gmail.com>
2022-07-14 20:06:05 +0530

    sweet: Welcome RiceDroid

--- lineage_sweet.mk
+++ lineage_sweet.mk
@@ -11,14 +11,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common RiceDroid stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_sweet
+PRODUCT_NAME := lineage_sweet
 
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
